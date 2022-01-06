7a78518a3a3abe09331e9197c7bbf2681f73ba9d
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2022-01-06 20:10:21 +0530

    Added

---
+++ 13.Decode the string.cpp
@@ -0,0 +1,40 @@
+class Solution{
+public:
+ string helper(string &s,int &index)
+    {
+     
+     string word="";
+        int n=0;
+        while(index<s.length()) 
+        {
+            if(s[index]>='0' && s[index]<='9')
+            {
+                n=(n*10)+s[index]-'0';
+            }
+            else if(s[index]=='[')
+            {
+                string next=helper(s,++index);
+                while(n>0)
+                {
+                    word+=next;
+                    n--;
+                }
+            }
+            else if(s[index]==']')
+            {
+                return word;
+            }
+            else
+            {
+                word.push_back(s[index]);
+            }
+            index++;
+        }
+        return word;
+    }
+    string decodedString(string s)
+    {
+        int index=0;
+        return helper(s,index);
+    }
+};
