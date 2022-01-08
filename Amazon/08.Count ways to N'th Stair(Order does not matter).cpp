716fdd28ad4bc92968069fde1fcf391de5843765
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2022-01-08 22:19:52 +0530

    Added

---
+++ Amazon/08.Count ways to N'th Stair(Order does not matter).cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        long long ans=m/2+1;
+        return ans;
+    }
+};
